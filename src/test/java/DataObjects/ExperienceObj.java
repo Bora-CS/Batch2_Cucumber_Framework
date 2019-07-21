733c3bedc19a82715984f6e7b253fb90789c091c
Ruslan Ibrahimov <ruslanibrahimoff@gmail.com>
2019-07-21 19:39:00 -0400

    Created ExperienceObj.java file

---
+++ src/test/java/DataObjects/ExperienceObj.java
@@ -0,0 +1,16 @@
+package DataObjects;
+
+public class ExperienceObj {
+	
+		public String title;
+		public String company;
+		public String from;
+
+		public ExperienceObj(String title, String company, String from) {
+			this.title = title;
+			this.company = company;
+			this.from = from;
+		}
+		
+		public ExperienceObj() {}
+	}
