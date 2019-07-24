2d6d2f8221a2203587c922c0e70af8560209276a
Muradil Erkin <Miller.muradil@gmail.com>
2019-07-23 20:43:10 -0400

    Helped Team Alpha

--- src/test/java/BoraAPIStepDefs/BoraAPI.java
+++ src/test/java/BoraAPIStepDefs/BoraAPI.java
@@ -1,4 +1,6 @@
 package BoraAPIStepDefs;
+import java.util.Map;
+
 import org.json.simple.JSONObject;
 
 import Utilities.Constants;
@@ -16,10 +18,32 @@ public class BoraAPI {
 		request.header("Content-Type", "application/json");
 		
 		JSONObject requestBody = new JSONObject();
-		requestBody.put("username", username);
+		requestBody.put("email", username);
 		requestBody.put("password", password);
 		request.body(requestBody);
 		
+		Response response = request.post(endpoint);
+		return response;
+	}
+
+	public static Response createProfile(String token, Map<String, String> profileInfo) {
+		String endpoint = "/api/profile";
+		RestAssured.baseURI = Constants.BORA_API_APPLICATION_URL;
+		RequestSpecification request = RestAssured.given();
+		request.header("Content-Type", "application/json");
+		request.header("Authorization", token);
+		
+		// 1st way to create json object from Map
+		JSONObject requestBody = new JSONObject(profileInfo);
+		request.body(requestBody);
+		
+//		// 2nd way to create json object from Map
+//		JSONObject requestBody = new JSONObject();
+//		for (String key : profileInfo.keySet()) {
+//			requestBody.put(key, profileInfo.get(key));
+//		}
+//		request.body(requestBody);
+
 		Response response = request.post(endpoint);
 		return response;
 	}
--- src/test/resources/FeatureFiles/CreateUserProfile.feature
+++ src/test/resources/FeatureFiles/CreateUserProfile.feature
@@ -3,15 +3,15 @@ Feature: Create user profle
   Description: Create user profile feature
 
   Scenario: Happy path
-		Given User is loged in to Bora API
-		When User pass in valid information
+		Given User is logged in via BoraTech APIs
+		When User pass in valid profile information
 			| handle   | Alpha             |
 			| company  | Bora Tech         |
 			| location | Fairfax, VA       |
-			| Status   | Automation Tester |
-			| Skills   | HTML, Java				 |
+			| status   | Automation Tester |
+			| skills   | HTML, Java				 |
 		Then User should get a 200 status code 
-#		And USer should see their profile updateS
+		And User should see their profile updates
 		
 
    
